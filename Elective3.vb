bd46e78a857dcb302df3db05374dde69c42d0db0
Eric Carable <workaholiric@gmail.com>
2015-12-13 19:58:33 +0800

    first app button control

---
+++ Elective3.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.Text = "First Application for Elective 3"
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        MessageBox.Show("Hello World!")
+    End Sub
+End Class
